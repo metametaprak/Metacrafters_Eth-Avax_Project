6d4183a97f65154aae348c7e3714c1eeaa71b516
metametaprak <22bis70002@cuchd.in>
2024-06-17 20:19:57 +0530

    Add files via upload

---
+++ Module_03_Types_Of_Functions/customERC.sol
@@ -0,0 +1,20 @@
+/ SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+     
+    string public tokenName = "Binance Coin";
+    string public tokenSymbol = "BNB";
+    
+    constructor() ERC20(tokenName, tokenSymbol) {}
+    
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+
+    function burn(uint256 _amount) public {
+        _burn(msg.sender, _amount);
+    }
+}
